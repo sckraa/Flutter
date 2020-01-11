b8a212fa0b98ba76127912a736368c790dfa9459
nelu <wf.nelu@yandex.ru>
2020-01-11 22:52:58 +0100

    Create class

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  Container (
-    color: Colors.red,
-    child: Center(
-      child: Text(
-        "Flutter",
-        textDirection: TextDirection.ltr, 
-        style: TextStyle(
-          fontSize: 72.0,
-          color: Colors.white
+  AppStatelessWidget()
+);
+
+class AppStatelessWidget extends StatelessWidget {
+  @override
+  Widget build( BuildContext context ) {
+    return Container(
+      color: Colors.red,
+      child: Center(
+        child: Text(
+          "Flutter",
+          textDirection: TextDirection.ltr, 
+          style: TextStyle(
+            fontSize: 72.0,
+            color: Colors.white
+          )
         )
       )
-    )
-  )
-);
+    );
+  }
+}
